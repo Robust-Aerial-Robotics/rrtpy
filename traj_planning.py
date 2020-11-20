9e148d0b21c175d4a48c6f8026b7f9ee40a33f7b
Carson Doering <cdoerin@purdue.edu>
2020-11-20 16:35:20 -0600

    Updated json file with more time in between:wq

--- traj_planning.py
+++ traj_planning.py
@@ -14,19 +14,19 @@ with open('rrt_result.csv', 'r') as f:
 M = []
 for line in lines:
     values = line.split(',')
-    row = [0,0,0,values[3]]
+    row1 = [0,0,0,values[3]]
     row = []
-
     for value in values:
         row.append(value)
     M.append(row)
 final_row = [row[0], row[1], 0, row[3]]
 M.append(final_row)
+M.insert(0, row1)
 
 waypoints = np.array(M)
     # x, y, z, yaw
 
-T = 2*np.ones(len(waypoints) - 1)
+T = 10*np.ones(len(waypoints) - 1)
 traj = tgen.min_snap_4d(waypoints, T, stop=True)
 res = traj.compute_inputs()
 print(res.max_data())
